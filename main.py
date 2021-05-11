c98e006629476260f8f40fa0d15aefdbacdfaf70
Sourav Datta <sdatta@planview.com>
2021-05-11 16:57:24 +0530

    Some new stuffs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests as req
 import datetime as dt
 import schedule
 import time
+from pynotifier import Notification
 
 API_HEAD = 'https://cdn-api.co-vin.in/api'
 
@@ -26,8 +27,22 @@ def get_districts(state_id):
     return {d['district_name']: d['district_id'] for d in district_res['districts']}
 
 
+def search_by_pin(pin, date):
+    endpoint = API_HEAD + '/v2/appointment/sessions/public/calendarByPin'
+    res = req.get(endpoint,
+                  params=dict(pincode=str(pin), date=date),
+                  headers={'User-Agent': 'PostmanRuntime/7.26.8'})
+    if not res.ok:
+        print(res.content)
+        raise Exception('get by pin failed')
+
+    return res.json()['centers']
+
+
 def show_session(sess):
     print('=' * 40)
+    print('PIN', sess['pincode'])
+    print('DATE', sess['date'])
     print(sess['name'])
     print(sess['address'])
     print(sess['fee_type'])
@@ -36,6 +51,30 @@ def show_session(sess):
     print(sess['vaccine'])
     print(sess['slots'])
     print('AGE', sess['min_age_limit'])
+    print('=' * 40)
+    print()
+
+
+def show_center(center):
+    print('=' * 40)
+    print(center['name'])
+    print(center['address'])
+    print(center['fee_type'])
+    print('From', center['from'])
+
+    for sess in center['sessions']:
+        print('SESSION')
+        print('Capacity', sess['available_capacity'])
+        if (sess['available_capacity'] != 0) and (sess['min_age_limit'] == 18):
+            Notification(
+                title='Slot AVAILABLE!!!',
+                description=center['name'],
+                duration=5,  # Duration in seconds
+                urgency='high'
+            ).send()
+        print(sess['slots'])
+        print('AGE', sess['min_age_limit'])
+
     print('=' * 40)
     print()
 
@@ -61,7 +100,6 @@ def get_appointments(states, dates, minage=45, district_substrings=None):
 
             print('\t', dname)
             for date in dates:
-                print('\t\t', date)
                 endpoint = API_HEAD + '/v2/appointment/sessions/public/findByDistrict'
                 res = req.get(endpoint,
                               params=dict(district_id=district_id,
@@ -71,6 +109,7 @@ def get_appointments(states, dates, minage=45, district_substrings=None):
                     sessions = res.json()['sessions']
                     for sess in sessions:
                         if sess['min_age_limit'] == minage:
+                            sess['date'] = date
                             show_session(sess)
                 else:
                     raise Exception(res.content)
@@ -90,17 +129,43 @@ def make_date_range(sdate, edate):
 def check():
     get_appointments(['Karnataka'],
                      make_date_range(
-                         (dt.datetime.today() + dt.timedelta(days=4)).strftime(
+                         (dt.datetime.today() + dt.timedelta(days=1)).strftime(
                              '%d-%m-%Y'),
                          (dt.datetime.today() + dt.timedelta(days=10)).strftime(
                              '%d-%m-%Y')),
-                     minage=18, district_substrings=None)
+                     minage=18, district_substrings=['bbmp'])
     print('*' * 20)
 
 
-if __name__ == '__main__':
-    schedule.every(10).minutes.do(check)
+def check_pincodes(pincodes, dates):
+    for pincode in pincodes:
+        print('PIN', pincode)
+        for date in dates:
+            print('DATE', date)
+            centers = search_by_pin(pincode, date)
+            for center in centers:
+                show_center(center)
 
+
+if __name__ == '__main__':
+    # schedule.every(2).minutes.do(lambda: check_pincodes(['560076'], make_date_range(
+    #     (dt.datetime.today() + dt.timedelta(days=1)).strftime(
+    #         '%d-%m-%Y'),
+    #     (dt.datetime.today() + dt.timedelta(days=14)).strftime(
+    #         '%d-%m-%Y'))))
+    #
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+
+    # check_pincodes(['560076'], make_date_range(
+    #     (dt.datetime.today() + dt.timedelta(days=1)).strftime(
+    #         '%d-%m-%Y'),
+    #     (dt.datetime.today() + dt.timedelta(days=14)).strftime(
+    #         '%d-%m-%Y')))
+
+    schedule.every(30).seconds.do(check)
     while True:
         schedule.run_pending()
         time.sleep(1)
+
